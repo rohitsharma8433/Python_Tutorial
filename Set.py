75375b987ab37490ac84506fd6a7b32c30f2bf2a
Rohit Sharma <122019511+rohitsharma8433@users.noreply.github.com>
2023-09-13 16:13:38 +0530

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,43 @@
+
+# In Python, a set is a built-in data structure that represents an unordered collection of unique elements. Sets are widely used in data structures and algorithms (DSA) for various purposes due to their efficiency in handling unique values. Here's an overview of sets in Python and how they can be used in DSA:
+
+
+# Creating Sets:
+# You can create a set in Python using curly braces {} or the set() constructor. Sets are unordered, so the order of elements is not guaranteed.
+
+
+# my_set = {'Ayush', "hrutika", 3,56}
+# print(my_set)
+
+
+# Operations on Sets:
+# Sets support various operations, including union, intersection, difference, and more. These operations can be useful in DSA when dealing with unique data.
+
+
+# set1 = {1, 2, 3,4,5}
+# set2 = {3, 4, 5,6,7}
+
+# # Union=set1.union(set2)
+# # print(Union)
+
+# Intersection=set1.intersection(set2) # its return common value in both set
+# print(Intersection)
+
+
+# Adding and Removing Elements:
+# You can add elements to a set using the add() method and remove elements using the remove() or discard() method. These operations can be handy when implementing algorithms that require adding or removing elements dynamically.
+
+
+# my_set = {1, 2, 3}
+# my_set.add(4)  # Add an element
+# my_set.remove(2)  # Remove an element
+
+# print(my_set)
+
+
+
+# Set using  loop 
+
+# my_set = {'Ayush', "hrutika", 3,56}
+# for i in(my_set):
+#     print(i)
