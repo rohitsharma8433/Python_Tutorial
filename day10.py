6c6c92acfea50f047c076d4fda6500a4523182d3
Rohit Sharma <122019511+rohitsharma8433@users.noreply.github.com>
2023-10-13 10:36:45 +0530

    Add files via upload

---
+++ day10.py
@@ -0,0 +1,50 @@
+'''
+In Python, a tuple is an ordered, immutable, and heterogeneous collection of elements. Tuples are similar to lists, but the key difference is that tuples cannot be modified once they are created. Here's how you can create and work with tuples in Python:'''
+
+# mytuple=()
+# print(type(mytuple))
+
+
+# Creating a tuple using parentheses
+# my_tuple = (101,101, 102, 103)
+
+# Creating a tuple without parentheses
+# another_tuple = 4, 5, 6
+# print(type(another_tuple))
+
+
+
+# Accessing Elements: 
+
+# print(my_tuple[0])
+
+# Slicing:
+
+# sliced_tuple = my_tuple[1:3]  # This creates a new tuple (102, 103)
+
+# print(sliced_tuple)
+
+#  loop through tuple
+
+# for i in my_tuple:
+#     print(i)
+
+
+
+# Tuple Methods
+
+# print(my_tuple.count(101))
+
+#  length of tuple 
+
+# print(len(my_tuple))
+
+# print(my_tuple.index(101))
+
+# Join Tuple 
+
+# tuple1 = ("Rohit", "Pragati" , "shubham",'karan')
+# tuple2 = (1, 2, 3)
+
+# NewTuple = tuple1 + tuple2
+# print(NewTuple)
