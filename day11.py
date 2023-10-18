983c6b2e14f6320d2325dc1ed21582915b6d70de
Rohit Sharma <122019511+rohitsharma8433@users.noreply.github.com>
2023-10-18 12:58:51 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,55 @@
+"""In Python, a set is a built-in data type that represents an unordered collection of unique elements. Sets are commonly used for tasks that involve checking for membership, eliminating duplicates, and performing set operations like union, intersection, and difference. You can create a set in Python using curly braces {} or by using the set() constructor"""
+
+
+# Creating a set
+# my_set = {1, 2, 3, 4, 5}
+# print(my_set[0])  not allowed 
+
+# print(my_set)
+
+# my_set = {1, 2, 3, 4, 5,5,6}
+# print(my_set) duplicate value not allowed 
+
+
+# Adding elements to a set
+# my_set.add(7)
+# print(my_set)
+
+
+# Removing elements from a set
+# my_set.remove(3)  # Raises an error if the element is not found
+
+# print(my_set)
+
+# my_set.discard(7)
+# print(my_set)
+
+
+#  loop through set
+# for i in my_set:
+#     print(i)
+
+
+# Set methods
+
+# print(len(my_set))
+
+# # union method
+# my_set1 = {1, 2, 3, 4, 5,5,6}
+# my_set2={7,8,9,10}
+
+# new_set=my_set1.union(my_set2)
+
+# print(new_set)
+
+
+
+# intersection
+
+# # union method
+# my_set1 = {1, 2, 3, 4,6}
+# my_set2={7,8,4,6}
+
+# new_set=my_set1.intersection(my_set2)
+
+# print(new_set)
